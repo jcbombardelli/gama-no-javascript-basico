4cbf80d7a4467d4e9632ed1eec418ff93749ba6a
vanessa <vanessa.win@hotmail.com>
2020-07-10 16:49:04 -0300

    validador CPF 2

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 console.log("javascript carregado");
 
+
+function validaCPF(cpf){
+	return false;
+}
+
+
 function validacao(){
 	console.log("Iniciando validação CPF");
+
+
+
+	var cpf  = document.getElementById('cpf_digitado').value; /*o cpf digitado vai ser capiturado atravez do  javascript dentro do html e vai ser armazenado dentro da variavel cpf*/
+	console.log(cpf) /*exibir valor do cpf*/
+	
+	var resultadoValidacao = validaCPF(cpf);
+	
+	if (resultadoValidacao) {
+		document.getElementById('success').style.display = "block";
+	}else{
+		document.getElementById('error').style.display = "block";
+	}
 }
