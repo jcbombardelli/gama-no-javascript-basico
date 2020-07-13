92c194c769332763e9c322834dd8b5a967be40e0
vanessa <vanessa.win@hotmail.com>
2020-07-13 19:14:37 -0300

    validador cpf 3cls

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-console.log("javascript carregado");
+console.log("javascript carregado"); 
 
-
-function validaCPF(cpf){
-	return false;
+function validaCPF(cpf){ //   essa função ffoi criada para saber se o cpf é verdadeiro falso, 
+	return false; // esse resultado esta sendo manipulado só para retornar  o resultado da funcao da lianha 14
 }
 
 
-function validacao(){
+function validacao() { 
+
 	console.log("Iniciando validação CPF");
 
+	var cpf = document.getElementById("cpf_digitado").value; //vai pegar o cpf digitado atraves do document.getElementById("cpf_digitado").value; depois gurardar o valor dentro de uma variavel chamada cpf0
+        
+		var resultadoValidacao = validaCPF(cpf);     //perguntar se a validaçao do cpf é verdadeiro
+
+		if (resultadoValidacao) {
+			document.getElementById("success").style.display = "block"; //pega o elemento html que esta ocuto pelo styl e transforma em vsivel
+
+		}
+		else {
+			document.getElementById("error").style.display = "block";  // retorna no html um texto que estava oculto.
+		}
 
 
-	var cpf  = document.getElementById('cpf_digitado').value; /*o cpf digitado vai ser capiturado atravez do  javascript dentro do html e vai ser armazenado dentro da variavel cpf*/
-	console.log(cpf) /*exibir valor do cpf*/
-	
-	var resultadoValidacao = validaCPF(cpf);
-	
-	if (resultadoValidacao) {
-		document.getElementById('success').style.display = "block";
-	}else{
-		document.getElementById('error').style.display = "block";
+
+
 	}
-}
